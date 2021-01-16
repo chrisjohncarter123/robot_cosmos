f07da90dc6c02f08f1912cec34e7744c27c0ed47
Chris Carter <chrisjohncarter123@gmail.com>
2021-01-16 07:54:54 -0600

    Worked on PartManipulator classes

--- Assets/Scripts/Robot/PartSelectorHit.cs
+++ Assets/Scripts/Robot/PartSelectorHit.cs
@@ -2,17 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace Robot{
 public class PartSelectorHit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    public void UpdatePositionRotationScale(PartSelectorSurface surface){
+
+        Part part = surface.GetPart();
+        PartType partType = part.GetPartType();
+
+        UpdatePosition(part.transform.position, surface.GetDirection(), partType.GetPartSelectorDistance());
+        UpdateRotation(part.transform.eulerAngles);
+        UpdateScale(partType.GetPartSelectorHitScale());
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void UpdatePosition(Vector3 partPosition, Vector3 direction, float distance){
+        transform.position = partPosition + (direction * distance);
+
     }
+    private void UpdateRotation(Vector3 eulerAngles){
+        transform.eulerAngles = eulerAngles;
+
+    }
+    private void UpdateScale(Vector3 scale){
+        transform.localScale = scale;
+
+    }
+
+}
 }
