69561429fe05cb9a3a576a36e87c5dd78d2e5f4f
Chris Carter <chrisjohncarter123@gmail.com>
2021-01-16 18:23:43 -0600

    Worked on Part Selection

---
+++ Assets/Scripts/Robot/Parts/Selector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Robot.Parts{
+    
+public class Selector : MonoBehaviour
+{
+    [SerializeField]
+    RobotManipulator robotManipulator;
+
+    [SerializeField]
+    PartType partType;
+
+    [SerializeField]
+    PartSelector partSelector;
+
+    void Start(){
+        partSelector.AddOnSelect(UsePart);
+        
+    }
+
+    public void UsePart(){
+        robotManipulator.AddPart(partType);
+
+    }
+}
+}
