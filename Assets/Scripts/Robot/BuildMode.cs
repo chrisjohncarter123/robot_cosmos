2af1205cfa77e844e7b64b00f966e27eee0cc3bb
Chris Carter <chrisjohncarter123@gmail.com>
2021-01-28 17:25:20 -0600

    Worked on Nodes

---
+++ Assets/Scripts/Robot/BuildMode.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildMode : MonoBehaviour
+{
+    [SerializeField] private string modeName;
+
+    [SerializeField] private string description;
+}
